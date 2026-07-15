b333edbeb2ea5ffaa3345bfc96cd1596cdb26fc7
benjamindrabo-dev <benjamindrabo@gmail.com>
2026-07-15 16:06:53 -0400

    Enable worldwide shipping in checkout process

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -45,8 +45,11 @@ export default async function handler(req, res) {
     // shopper here after payment; the webhook creates the Shopify order.
     params.append("return_url", (process.env.SUCCESS_URL || "https://example.com/thank-you") + "?session_id={CHECKOUT_SESSION_ID}");
     params.append("billing_address_collection", "auto");
-    params.append("shipping_address_collection[allowed_countries][0]", "CA");
-    params.append("shipping_address_collection[allowed_countries][1]", "US");
+    // Worldwide shipping: every country Stripe supports for shipping addresses.
+    // (The 4 sanctioned countries CU/IR/KP/SY are omitted, plus RU.) To restrict
+    // where you sell, trim this list (e.g. keep only "CA US GB ...").
+    const SHIP_COUNTRIES = ("AC AD AE AF AG AI AL AM AO AQ AR AT AU AW AX AZ BA BB BD BE BF BG BH BI BJ BL BM BN BO BQ BR BS BT BV BW BY BZ CA CD CF CG CH CI CK CL CM CN CO CR CV CW CY CZ DE DJ DK DM DO DZ EC EE EG EH ER ES ET FI FJ FK FO FR GA GB GD GE GF GG GH GI GL GM GN GP GQ GR GS GT GU GW GY HK HN HR HT HU ID IE IL IM IN IO IQ IS IT JE JM JO JP KE KG KH KI KM KN KR KW KY KZ LA LB LC LI LK LR LS LT LU LV LY MA MC MD ME MF MG MK ML MM MN MO MQ MR MS MT MU MV MW MX MY MZ NA NC NE NG NI NL NO NP NR NU NZ OM PA PE PF PG PH PK PL PM PN PR PS PT PY QA RE RO RS RW SA SB SC SD SE SG SH SI SJ SK SL SM SN SO SR SS ST SV SX SZ TA TC TD TF TG TH TJ TK TL TM TN TO TR TT TV TW TZ UA UG US UY UZ VA VC VE VG VN VU WF WS XK YE YT ZA ZM ZW").split(" ");
+    SHIP_COUNTRIES.forEach((c, i) => params.append(`shipping_address_collection[allowed_countries][${i}]`, c));
 
     items.forEach((it, i) => {
       params.append(`line_items[${i}][price_data][currency]`, CUR);
